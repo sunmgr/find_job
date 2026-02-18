eecac5d8856fc4e6a5a3d61bbc67fa79a6807bbf
sunmgr <136238285+sunmgr@users.noreply.github.com>
2026-02-18 22:53:21 +0545

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://find-job-backend-7cyd.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions))
