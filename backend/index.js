03ab3325da4e3b3d5bde8d09bd603438c2da1fec
sunash magar <magarsunash005@gmail.com>
2026-02-19 00:03:43 +0545

    fix: update wildcard

--- backend/index.js
+++ backend/index.js
@@ -43,15 +43,14 @@ app.use("/api/v1/company",companyRoute)
 app.use("/api/v1/job",jobRoute)
 app.use("/api/v1/application",applicationRoute)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
     
-   
-    app.get("(.*)", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    });
+    // Change (.*) or * to /:any*
+    app.get("/:any*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")) 
+    })
 }
-
 app.listen(PORT,()=>{
     connectDB()
     console.log(`Server running at port ${PORT}`)
