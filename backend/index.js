8a51514e95e58907868a71f151d7bcca96bbedd0
sunash magar <magarsunash005@gmail.com>
2026-02-18 23:32:53 +0545

    last commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/job.route.js"
 import applicationRoute from "./routes/application.route.js"
-
+import  path from "path"
 
 
 dotenv.config({})
@@ -21,7 +21,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
 const app = express()
-
+const __dirname=path.resolve()
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -42,6 +42,14 @@ app.use("/api/v1/user",userRoute)
 app.use("/api/v1/company",companyRoute)
 app.use("/api/v1/job",jobRoute)
 app.use("/api/v1/application",applicationRoute)
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html")) 
+})
+}
 
 
 app.listen(PORT,()=>{
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,6 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user"
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company"
+
+export const USER_API_END_POINT = import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1/user" : "/api/v1/user"
+export const JOB_API_END_POINT = import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1/job" : "/api/v1/job"
+export const APPLICATION_API_END_POINT = import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1/application" : "/api/v1/application"
+export const COMPANY_API_END_POINT = import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1/company" : "/api/v1/company"
 
